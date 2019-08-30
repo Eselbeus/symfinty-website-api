1c0b5b02f0f587141296909b092988e3a17e56ed
github username <github email address>
2019-08-30 03:23:06 -0400

    posts controller working and 1st post created

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,2 +1,24 @@
 class Api::V1::PostsController < ApplicationController
+  def index
+    @posts = Post.all
+    render :index
+  end
+
+  def show
+    @post = User.find(params[:id])
+    render :show
+  end
+
+  def create
+    @user = User.find(post_params[:user_id])
+    @post = Post.create(user_params)
+    @user.posts << @post
+    render :create
+  end
+
+  private
+
+  def post_params
+    params.permit(:id, :headline, :body, :author, :user_id)
+  end
 end
