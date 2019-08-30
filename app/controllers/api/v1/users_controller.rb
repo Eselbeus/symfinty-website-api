f9d25bd9592e2e9166bc27ba475de133552d7c85
github username <github email address>
2019-08-30 02:42:00 -0400

    building user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,22 @@
 class Api::V1::UsersController < ApplicationController
+
+  def index
+    @users = User.all
+    render :index
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render :show
+  end
+
+  def create
+    @user = User.create(user_params)
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :username, :email, :password, :link)
+  end
 end
