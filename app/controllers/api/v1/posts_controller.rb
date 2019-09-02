96a862e803781b5236ca53b7dc0b0751cd64e9ef
github username <github email address>
2019-09-02 12:11:45 -0400

    json viewable on frontend

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,19 +1,19 @@
 class Api::V1::PostsController < ApplicationController
   def index
     @posts = Post.all
-    render :index
+    render json: @posts
   end
 
   def show
     @post = User.find(params[:id])
-    render :show
+    render json: @post
   end
 
   def create
     @user = User.find(post_params[:user_id])
     @post = Post.create(user_params)
     @user.posts << @post
-    render :create
+    render json: @post
   end
 
   private
--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,3 +1,3 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :headline, :body, :author, :user_id
 end
